b7368e8835730f83dfb44c2aa019e569a0085d97
Leo <conceicao.leonardo9@gmail.com>
2023-01-30 12:05:08 -0300

    usando a camada factory ao inves da camada fixtures

--- cypress/e2e/cadastro.cy.js
+++ cypress/e2e/cadastro.cy.js
@@ -1,19 +1,22 @@
 import signup from "../pages/SignupPage"
+import signupFactory from "../factories/SignupFactory"
 
 describe('Cadastro', ()=>{
 
-    beforeEach(function() {
+    /*beforeEach(function() {
         cy.fixture('deliver').then((d)=>{
             this.deliver = d
         })
-    });
+    });*/
 
     it('Usuario deve se tornar um entregador', function() {
         
+        var deliver = signupFactory.deliver()
+
         const expectedMessage = 'Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato.'
 
         signup.go()
-        signup.filform(this.deliver.signup)
+        signup.filform(deliver)
         signup.submit()
         signup.modalContaintShouldBe(expectedMessage)
 
@@ -21,20 +24,29 @@ describe('Cadastro', ()=>{
 
     it('Cpf incorreto', function() {
         
+        var deliver = signupFactory.deliver()
+
+        deliver.cpf = '00000141aa'
+
         const texto_erro = 'Oops! CPF inválido'
 
         signup.go()
-        signup.filform(this.deliver.cpf_inv)
+        signup.filform(deliver)
         signup.submit()
         signup.alertExpectedMessage(texto_erro)
 
     });
 
     it('Email incorreto', function() {
+        
+        var deliver = signupFactory.deliver()
+
+        deliver.email = 'lucas.com.br'
+        
         const texto_erro = 'Oops! Email com formato invalido.'
 
         signup.go()
-        signup.filform(this.deliver.email_inv)
+        signup.filform(deliver)
         signup.submit()
         signup.alertExpectedMessage(texto_erro)
     });
---
+++ cypress/factories/SignupFactory.js
@@ -0,0 +1,28 @@
+
+
+ export default {
+
+    deliver: function() {
+        
+        var data = {
+            nome: 'Leon',
+                cpf: '12333355505',
+                email: 'leon.sora@gmail.com',
+                whatsapp: '11984637897',
+                endereco: {
+                    cep: '04534011',
+                    rua: 'Rua Joaquim Floriano',
+                    numero: '1000',
+                    complemento: 'apt 141',
+                    bairro: 'Itaim Bibi',
+                    cidade_uf: 'São Paulo/SP'
+                },
+                metodo_entrega: 'Bicicleta',
+                cnh: 'cnh_gratuita.jpg'
+        }
+
+        return data
+    }
+
+
+ }
